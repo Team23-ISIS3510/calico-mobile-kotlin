f9bddc722a528d4d85b020d25ea12c17819dc786
Nikol Rodriguez <nk.rodriguez@uniandes.edu.co>
2026-05-12 17:57:34 -0500

    client id

--- app/src/main/java/com/calico/tutor/LoginActivityExample.kt
+++ app/src/main/java/com/calico/tutor/LoginActivityExample.kt
@@ -33,8 +33,8 @@ class LoginActivityExample : AppCompatActivity() {
 
     private lateinit var viewModel: AuthViewModel
     private val googleSignInManager by lazy {
-        // TODO: Reemplaza con tu Web Client ID
-        GoogleSignInManager(this, "YOUR_WEB_CLIENT_ID")
+        val googleClientId = BuildConfig.GOOGLE_CLIENT_ID_ANDROID
+        GoogleSignInManager(this, googleClientId)
     }
 
     // Launcher para iniciar Google Sign-In
